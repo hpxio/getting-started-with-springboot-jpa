81ba2bb67b7588be4a69c82628c0ad367f5ee414
HPxIO <hpx.io@keemail.me>
2022-05-01 01:28:24 +0530

    init-project

---
+++ src/main/java/com/app/hpx/jpa/GettingStartedWithSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.app.hpx.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/hpx/jpa/GettingStartedWithSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hpx.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
